8ad59a4df674c38c13c240e083eb068dd9905af2
Sterniko <33185467+Sterniko@users.noreply.github.com>
2018-12-17 10:22:08 +0100

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -21,7 +21,7 @@ def train_network():
 
     # get amount of pitch names
     print(set(notes))
-    print(str(music21.note.Rest))
+    print(str(note.Rest()))
     n_vocab = len(set(notes))
 
     network_input, network_output = prepare_sequences(notes, n_vocab)
